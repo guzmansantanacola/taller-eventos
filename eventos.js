aa3c2221ec6cef987950e6866cefdd5f55550a5c
guzmansantanacola <guzmansantanacola@gmail.com>
2023-08-22 01:46:05 -0300

    commit

--- eventos.js
+++ eventos.js
@@ -2,5 +2,12 @@ function saludarDesdeDiv() {
     alert("Hola! Soy el div");
 }
 
-const divElement = document.querySelector("div");
-divElement.addEventListener("click", saludarDesdeDiv);
+const divElement = document.getElementById("btn");
+divElement.addEventListener("click", saludarDesdeDiv); 
+
+
+const botonElement = document.getElementById("saludarBtn");
+botonElement.onclick = function(event) {
+    alert("Hola!");
+    event.stopPropagation();
+};
